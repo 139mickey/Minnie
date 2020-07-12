cf0e75b7d45fd69f7cc4ecb18285fed320645d84
139mickey <550695@qq.com>
2020-07-12 20:24:37 +0800

    Clear up unused files

--- public/test.php
+++ public/test.php
@@ -1,1 +1,43 @@
 <?php
+abstract class Animal {
+    var $name = "I'm Animal ";
+
+    public function __construct($name = null){
+        $this->name = $name ? $name : $this->name;
+    }
+
+    public function foodType(){
+        echo "I like anything ";
+    }
+
+    public function whoAmI(){
+        echo $this->name;
+    }
+}
+
+trait Carnivore {
+    var $someTrait = "here is some trait";
+        public function whoAmI(){
+            echo "I like burger ";
+        }
+}
+
+class Cat extends Animal {
+
+    var $someTrait = "here is some strait";
+    use Carnivore;
+
+    public function wwhoAmI(){
+        echo "I am a cat ";
+    }
+
+    public function foodType(){
+        echo "I like finish ";
+        echo "and i like ". $this->someTrait;
+    }
+
+}
+
+$cat = new Cat();
+$cat->whoAmI();
+$cat->foodType();
